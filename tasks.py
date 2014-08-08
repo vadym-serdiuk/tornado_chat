6f4a846b6fe5ae5a1bd2227a5741551575a012b3
Vadym Serdyuk <va.serdyuk@gmail.com>
2014-08-08 13:48:35 +0300

    rabbit connection added

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 __author__ = 'vserdyuk'
 
@@ -7,16 +8,24 @@ import pika
 EXCHANGE_NAME = 'exchange'
 EXCHANGE_TYPE = 'direct'
 QUEUE = 'screenshots'
-ROUTING_KEY = 'completed'
+
+START_ROUTING_KEY = 'get'
+CHECK_ROUTING_KEY = 'check'
 
 rabbit_url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2F')
+
 connection = pika.BlockingConnection(pika.URLParameters(rabbit_url))
 channel = connection.channel()
 
 channel.queue_declare(queue=QUEUE)
+def callback(ch, method, properties, body):
+    print " [x] Received %r %r" % (body, method)
+    time.sleep( 5 )
+    print " [x] Done"
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_publish(exchange=EXCHANGE_NAME,
-                      routing_key=ROUTING_KEY,
-                      body='Hello World!')
-print " [x] Sent 'Hello World!'"
-connection.close()
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue=QUEUE)
+
+channel.start_consuming()
